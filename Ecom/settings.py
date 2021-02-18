ab8c9fbde2cc4e2935248ed7d9d2b0d5b29ef634
Gaurav Girase <gaurav.girase111@gmail.com>
2021-02-18 19:09:11 +0530

    modifiedsetting

--- Ecom/settings.py
+++ Ecom/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'eook)&o8kk%z-70=ldq$a^#x$r($7_^*24yj%vacvf%51tx#o$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://dashboard.heroku.com/apps/shoppingcartdemo2','http://127.0.0.1:8000/']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -51,7 +52,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     # 'store.middlewares.auth.auth_middleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 
 ]
 
@@ -124,11 +124,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-# STATIC_ROOT = BASE_DIR
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
-# MEDIA_ROOT = BASE_DIR
-MEDIA_URL ="/abcd/"
+MEDIA_URL ="/images/"
+STATICFILES_DIRS = [
+        os.path.join(BASE_DIR,'static')
+]
+MEDIA_ROOT = os.path.join(BASE_DIR,'static/images')
+
 django_heroku.settings(locals())
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
