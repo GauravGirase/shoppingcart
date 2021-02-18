62969ae5a0a7972614eb7ae69a9f621b3a2185c3
Gaurav Girase <gaurav.girase111@gmail.com>
2021-02-18 20:21:01 +0530

    add procfile

--- Ecom/settings.py
+++ Ecom/settings.py
@@ -125,9 +125,9 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
-MEDIA_URL ='/images/'
+MEDIA_URL ='/abcd/'
 STATICFILES_DIRS = [
         os.path.join(BASE_DIR,'static')
 ]
-MEDIA_ROOT = os.path.join(BASE_DIR,'static/images')
+MEDIA_ROOT = BASE_DIR
 
