44a3449e6010b39538bb3ba763b92aeca086535c
Gaurav Girase <gaurav.girase111@gmail.com>
2021-02-18 17:28:17 +0530

    database settings

--- store/models.py
+++ store/models.py
@@ -24,7 +24,7 @@ class Customer(models.Model):
     firstname = models.CharField(max_length=50)
     lastname = models.CharField(max_length=50)
     email = models.EmailField(max_length=50)
-    mobile = models.IntegerField()
+    mobile = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
 
 
